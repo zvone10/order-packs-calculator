ce155a005310b99d72c7e26611fff79669399bf8
zvone10 <iliczvonimir@gmail.com>
2026-02-27 11:59:04 +0100

    feat: add port environment variable for configuration

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 	"order-pack-calculator-api/internal/calculator"
 	"order-pack-calculator-api/internal/handler"
 	"order-pack-calculator-api/internal/service"
 
 	"github.com/gin-gonic/gin"
+	"github.com/kelseyhightower/envconfig"
 )
 
+type Config struct {
+	Port int `envconfig:"PORT" default:"8080"`
+}
+
 func main() {
+	var cfg Config
+	if err := envconfig.Process("", &cfg); err != nil {
+		log.Fatalf("Failed to process environment variables: %v", err)
+	}
+
 	calculator := calculator.NewCalculator()
 	svc := service.NewPackingService(calculator)
 	h := handler.NewPackingHandler(svc)
@@ -22,5 +34,6 @@ func main() {
 	v1 := r.Group("/api/v1")
 	v1.POST("/pack-calculation", h.Calculate)
 
-	r.Run(":8080")
+	addr := fmt.Sprintf(":%d", cfg.Port)
+	r.Run(addr)
 }
